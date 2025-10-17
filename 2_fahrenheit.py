85e7950e56ffab87a22e993e570c8ed78b6eb54a
ANAMONTEIROMISQUITA <anaclaudia.misquita@gmail.com>
2025-10-17 11:49:45 -0300

    update_fahrenheit

--- 2_fahrenheit.py
+++ 2_fahrenheit.py
@@ -6,3 +6,11 @@ fahrenheit = float(input("Digite a temperatura em graus Fahrenheit: "))
 celsius = (fahrenheit - 32) * 5 / 9
 
 print(f"A temperatura em Celsius é: {celsius:.2f}°C")
+
+## De Celsius para Fahrenheit
+
+# celsius = float(input("Digite a temperatura em graus Celsius: "))
+
+# fahrenheit = (celsius * 9 / 5) + 32
+
+# print(f"A temperatura em Fahrenheit é: {fahrenheit:.2f}°F")
