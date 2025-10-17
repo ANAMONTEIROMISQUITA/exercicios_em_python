e692d193da075695e419ec68bad72ef5231d3dd3
ANAMONTEIROMISQUITA <anaclaudia.misquita@gmail.com>
2025-10-17 11:49:12 -0300

    update_fahrenheit

--- 2_fahrenheit.py
+++ 2_fahrenheit.py
@@ -1,2 +1,8 @@
 # 2. Faça um Programa que peça a temperatura em graus Fahrenheit,
 # transforme e mostre a temperatura em graus Celsius.
+
+fahrenheit = float(input("Digite a temperatura em graus Fahrenheit: "))
+
+celsius = (fahrenheit - 32) * 5 / 9
+
+print(f"A temperatura em Celsius é: {celsius:.2f}°C")
