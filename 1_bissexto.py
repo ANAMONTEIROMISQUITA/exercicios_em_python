02e2255431f0cd0279bf6bbebb0628475aaf1cab
ANAMONTEIROMISQUITA <anaclaudia.misquita@gmail.com>
2025-10-17 11:46:26 -0300

    update bissexto

---
+++ 1_bissexto.py
@@ -0,0 +1,7 @@
+# 1. Crie um programa que determine se um ano é bissexto com base nas
+# regras do calendário gregoriano. Seu programa deve receber um ano como
+# entrada e verificar se ele atende às seguintes condições:
+# - Um ano é bissexto se for divisível por 4, exceto se também for divisível por 100.
+# - Entretanto, se o ano for divisível por 100 e por 400, ele será bissexto.
+# Com base nessas regras, implemente a lógica e exiba se o ano fornecido é
+# bissexto ou não.
