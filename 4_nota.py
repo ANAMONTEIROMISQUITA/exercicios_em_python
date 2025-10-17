88184156120c0cb7e425a4ab56d9a298ca06a20a
ANAMONTEIROMISQUITA <anaclaudia.misquita@gmail.com>
2025-10-17 11:52:27 -0300

    update nota

--- 4_nota.py
+++ 4_nota.py
@@ -3,3 +3,17 @@
 # A mensagem Aprovado, se a média alcançada for maior ou igual a sete;
 # A mensagem Reprovado, se a média for menor do que sete;
 # A mensagem Aprovado com Distinção&quot, se a média for igual a dez.
+
+nota1 = float(input("Digite a primeira nota: "))
+nota2 = float(input("Digite a segunda nota: "))
+
+media = (nota1 + nota2) / 2
+
+if media == 10:
+    print("Aprovado com Distinção")
+elif media >= 7:
+    print("Aprovado")
+else:
+    print("Reprovado")
+
+print(f"Sua média foi: {media:.2f}")
