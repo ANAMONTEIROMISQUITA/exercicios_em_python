ae545ba6bf5a15246195a48e8edacb00f932980a
ANAMONTEIROMISQUITA <anaclaudia.misquita@gmail.com>
2025-10-17 11:57:39 -0300

    update craps

---
+++ 8_craps.py
@@ -0,0 +1,10 @@
+# 8. Jogo de Craps: Crie um programa que simule um jogo de Craps. O jogo funciona da seguinte maneira:
+# O jogador lança dois dados, resultando em um valor entre 2 e 12.
+# Na primeira jogada:
+# - Se o resultado for 7 ou 11, o jogador vence automaticamente (chamado de 'natural').
+# - Se o resultado for 2, 3 ou 12, o jogador perde imediatamente (chamado de 'craps').
+# - Se o resultado for 4, 5, 6, 8, 9 ou 10, esse valor se torna o Ponto do jogador.
+# Caso um 'Ponto' tenha sido estabelecido, o jogador continua lançando os dados:
+# - Se tirar o mesmo valor do Ponto, ele vence.
+# - Se tirar um 7 antes de repetir o Ponto, ele perde.
+# Implemente a lógica para simular esse jogo e exiba o resultado de cada jogada.
